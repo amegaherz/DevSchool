a4d2361f4722e1f592f5d06cdab5df01a9cf373d
MHz <a.megaherz@gmail.com>
2019-02-28 15:48:12 +0300

    restructuring solution

--- DigDes.Reflection/DigDes.Reflection/Program.cs
+++ DigDes.Reflection/DigDes.Reflection/Program.cs
@@ -1,13 +1,48 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using DigDes.Reflection.Model;
 
 namespace DigDes.Reflection {
 	class Program {
 		static void Main(string[] args) {
-			IlExample();
+			LinqExample();
+			//IlExample();
+		}
+
+		private static void LinqExample() {
+
+			var w = Stopwatch.StartNew();
+			var arr = new int[] {
+				10000,2,3,4,5,6,7,10
+			};
+
+			var min = arr.Min();
+			var max = arr.Max();
+			var buf2 = Enumerable.Range(min, max);
+
+			buf2 = buf2.Except(arr).ToArray();
+
+
+			//Array.Sort(arr);
+
+			////if (arr[arr.Length -1] - arr[0])
+
+
+			//var buf = new List<int>(arr[arr.Length - 1] - arr[0] - arr.Length + 1);
+			//for (int i = 1; i < arr.Length; i++) {
+			//	if (arr[i] - arr[i - 1] > 1) {
+			//		for (int j = 1; j <= arr[i] - arr[i - 1] - 1; j++) {
+			//			buf.Add(arr[i - 1] + j);
+			//		}
+			//	}
+			//}
+			w.Stop();
+			return;
 		}
 
 		private static void IlExample() {
@@ -32,5 +67,12 @@ namespace DigDes.Reflection {
 			var obj = Activator.CreateInstance(abstrType);
 			
 		}
+
+
+
+
+
 	}
+	
+
 }
