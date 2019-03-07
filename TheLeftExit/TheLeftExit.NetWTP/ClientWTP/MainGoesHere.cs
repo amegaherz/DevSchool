0b3cdab2891821449ad8be4f3e81e3473a240336
TheLeftExit <47351802+TheLeftExit@users.noreply.github.com>
2019-03-07 19:15:36 +0300

    Решение разбито на несколько. Добавлен TheLeftExit.NetWTP.

---
+++ TheLeftExit/TheLeftExit.NetWTP/ClientWTP/MainGoesHere.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ClientWTP
+{
+    class MainGoesHere
+    {
+        public static void Main(string[] args)
+        {
+            // Если запускаем лениво и/или из VS, задаём параметры в интерактивном режиме.
+            if(args.Length == 0)
+            {
+                args = new string[3];
+                Console.Write("Что используем для конвертации (-mm или -word): ");
+                args[0] = Console.ReadLine();
+                Console.Write("Входной файл: ");
+                args[1] = Console.ReadLine();
+                Console.Write("Выходной файл: ");
+                args[2] = Console.ReadLine();
+            }
+
+            // Чтоб не было исключений.
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Неизвестный формат ввода.");
+                return;
+            }
+            if (args[0] != "-mm" && args[0] != "-word")
+            {
+                Console.WriteLine("Неизвестный режим работы.");
+                return;
+            }
+            if (!File.Exists(args[1]))
+            {
+                Console.WriteLine("Входной файл не найден.");
+                return;
+            }
+
+            byte[] inbin = File.ReadAllBytes(args[1]);
+            WTP.WTPResponse response = null;
+
+            Console.WriteLine("Операция выполняется...");
+
+            using (var client = new WTP.Service1Client())
+                switch (args[0].ToLower())
+                {
+                    case "-mm":
+                        response = client.ConvertWithMM(inbin);
+                        break;
+                    case "-word":
+                        response = client.ConvertWithWord(inbin);
+                        break;
+                }
+
+            if (response.success)
+            {
+                try
+                {
+                    File.WriteAllBytes(args[2], response.binary);
+                }
+                catch
+                {
+                    Console.WriteLine("Нет доступа к рабочей папке. Нажмите любую клавишу...");
+                    Console.ReadKey();
+                    return;
+                }
+                Console.WriteLine("Операция завершена. Нажмите любую клавишу...");
+            }
+            else
+            {
+                Console.WriteLine($"Операция прервана. Нажмите любую клавишу...\n{response.header}");
+            }
+
+            Console.ReadKey();
+            return;
+        }
+    }
+}
---
+++ TheLeftExit/TheLeftExit.NetWTP/ServerWTP/IService1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using SautinSoft;
+
+namespace ServerWTP
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        WTPResponse ConvertWithMM(byte[] binary);
+
+        [OperationContract]
+        WTPResponse ConvertWithWord(byte[] binary);
+    }
+    
+    [DataContract]
+    public class WTPResponse
+    {
+        [DataMember]
+        public string header;
+
+        [DataMember]
+        public bool success;
+
+        [DataMember]
+        public byte[] binary;
+        
+        public WTPResponse(byte[] bin)
+        {
+            header = "A-OK";
+            success = true;
+            binary = bin;
+        }
+
+        public WTPResponse(string message)
+        {
+            header = message;
+            success = false;
+        }
+    }
+
+}
